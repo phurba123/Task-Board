75eb690c71a62177c76bde4e5fac483fbee3f111
PhursanG <psherpa592@gmail.com>
2022-10-10 18:16:58 +0530

    material inclusion successfull

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar'
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatIconModule,
+    MatToolbarModule
+  ],
+  exports:[
+    MatIconModule,
+    MatToolbarModule
+  ]
+})
+export class MaterialModule { }
